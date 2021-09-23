eb54f0820afe412b7f61c944566cde6e43380305
Thom Versigny <thomversigny@gmail.com>
2021-09-23 10:09:32 +0200

    code edited/added

--- TheBackrooms.py
+++ TheBackrooms.py
@@ -3,6 +3,7 @@ import sys
 import time
 import os
 import winsound
+crazy = 0
 
 print("""
 ████████╗██╗░░██╗███████╗
@@ -22,12 +23,8 @@ time.sleep(5)
 os.system("cls")
 
 string = """It was approximately 12:15 when I entered the Johnson County Community Health Clinic. 
-I was there for an appointment I had set up weeks ago, just a routine checkup. It wasn’t a new place for me; I had been there a couple times before.
-I passed the empty waiting area a small area of the main room with magazines, children’s playthings and blue cushioned chairs— and approached the woman at the front desk.
-She was sitting in her blueish gray office chair, looking at a spreadsheet on the same Windows XP desktop they’ve had since 2008.
-There was a sign-in sheet on the counter in front of me.
-
-“I have an appointment with Dr. Pebins?” I asked.
+I was there for an appointment I had set up weeks ago, just a routine checkup.
+I passed the empty waiting area a small area, and approached the woman at the front desk.“I have an appointment with Dr. Pebins?” I asked.
 “What time?”
 “12:30,” I replied.
 She began typing something into her keyboard.
@@ -42,10 +39,12 @@ And my fingers went through it. I recoiled in shock.
 “What the hell was that?” I thought, as I reached to touch the wall again, only to find my fingers clipped through once more.
 Then, suddenly, I lost my balance, tripped, and fell directly through the wall. I fell face-first onto some dirty tan carpeting.
 Upon getting back up, I realized that I was in a completely different room. Well, not really a room, per se more so a set of rooms, all of which connected by openings.
-The walls were covered in gross tan patterned wallpaper. There was also an overwhelming stench of moist carpet."""
+The walls were covered in gross tan patterned wallpaper. There was also an overwhelming stench of moist carpet.
+In time your Sanity Meter goes slowly up, how lower the meter the better the chance to survive.
+Press sometimes D to repeat your fine."""
 
 for letter in string:
-  sleep(0.05)
+  sleep(0.045)
   sys.stdout.write(letter)
   sys.stdout.flush()
 
@@ -54,14 +53,44 @@ os.system("cls")
 
 winsound.PlaySound("hey.wav", winsound.SND_ASYNC + winsound.SND_LOOP | winsound.SND_ALIAS )
 
-start = input("""What are you going to do?
+choice = input("""What are you going to do?
 A(Search the room for clues
 B(Try to go through the wall
 C(Do nothing: """).lower()
 
-if start == 'a':
+if choice == 'a':
   print("You found nothing.")
-elif start == 'b':
+elif choice == 'b':
   print("Your hand wont go through the wall. You realize your stuck in this room")
-elif start == 'c':
+  crazy += 1
+elif choice == 'c':
   print("You did nothing.")
+
+while crazy <= 10:
+  if crazy == 0:
+    choice = input("""What are you going to do?
+    A(Try to search the other rooms
+    B(Walk through the rooms
+    C(Do nothing: """).lower()
+    if choice == 'a':
+      print("You searched the other room, But you found nothing.")
+    elif choice == 'b':
+      print("You walk room after room but nothing is changing")
+      crazy += 1
+    elif choice == 'c':
+      print("You did nothing.")
+  elif crazy == 1:
+    choice = input("""You think your going crazy.
+    A(Repeat your fine.
+    B(Walk too the other rooms
+    C(Do nothing: """).lower()
+    if choice == 'a':
+      print("You repeated im fine. Your sanity goes back to normal.")
+      print("In time your Sanity Meter goes slowly up, press sometimes D to repeat your fine.")
+      crazy -= 1
+    elif choice == 'b':
+      print("You walk room after room but nothing is changing")
+      crazy += 1
+    elif choice == 'c':
+      print("You did nothing.")
+      crazy += 1
